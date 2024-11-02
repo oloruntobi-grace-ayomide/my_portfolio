4072b55f238cdde2abc532833c265f9ce7600ad5
graceayomi <oayomideg@gmail.com>
2024-11-02 14:16:36 +0100

    Solving deployment error

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
@@ -7,6 +8,9 @@ mail = Mail()
 
 def create_app():
     app=Flask(__name__, instance_relative_config=True)
+
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
     app.config.from_pyfile('config.py', silent=True)
 
     csrf.init_app(app)
